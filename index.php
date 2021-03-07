c98933da4e1bdf3a07103f6aa287d67659f69a1e
MDubovac <mladendubovac98@gmail.com>
2021-03-07 21:47:17 +0100

    Added some CSS

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
     require_once "./form-handlers/login-handler.php";
 ?>
 
-<h1><?php echo isset($_SESSION["auth_user"]) ? "Welcome " . $_SESSION["auth_user"] : "Welcome"; ?></h1>
+<div id="home" class="py-3 px-5">
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to the Application</h1>
+        <p class="lead">
+            This is a starter template for PHP Devs. It comes with Login System, Bootstrap and Font Awesome.
+        </p>
+        <hr class="my-4">
+        <p>
+            This project is Open-Source, so feel free to tinker with it.
+        </p>
+        <p class="lead">
+            <a class="btn btn-primary" href="login.php" role="button">Log In</a>
+        </p>
+    </div>
+</div>
 
-   
+
 <?php 
     require_once "./inc/footer.php";
 ?>
